65263647b7a56a9502878c3fa800d40267b45df8
Ankita Patangay <akshaypatange@Akshays-MacBook-Pro.local>
2017-11-24 22:27:38 +0530

    added teamStats controller

--- angular/controllers/teamStatsController.js
+++ angular/controllers/teamStatsController.js
@@ -1,70 +1,125 @@
-app.controller("teamStatsController", ['$http', function($http) {  debugger;
-    var main = this;
-    this.stats = {};
-    this.getStats = function() {
-        function getTeamStats(response) {
-            data = response.data.rounds;
-            var stats = main.stats;
-            for (var i in data) {
-                for (var j in data[i].matches) {
-                    var match = data[i].matches[j];
-                    var team1 = match.team1.code;
-                    var team2 = match.team2.code;
+ app.controller('teamStatsController', ['$http', function($http) {
+     var main = this;
+     this.obj1={};
+     this.obj2={};
 
-                    if (stats[team1]) {
-                        stats[team1].goals += match.score1;
-                        stats[team1].matchesPlayed++;
-                    } else {
-                        stats[team1] = {};
-                        stats[team1].goals = match.score1;
-                        stats[team1].matchesPlayed = 1;
-                        stats[team1].wins = 0
-                        stats[team1].losses = 0;
-                    }
-                    if (stats[team2]) {
-                        stats[team2].goals += match.score2;
-                        stats[team2].matchesPlayed++;
-                        stats[team2].wins = 0
-                        stats[team2].losses = 0;
-                    } else {
-                        stats[team2] = {};
-                        stats[team2].goals = match.score2;
-                        stats[team2].matchesPlayed = 1;
-                    }
-                    if (match.score1 > match.score2) {
-                        stats[team1].wins++;
-                        stats[team2].losses++;
-                    } else {
-                        stats[team2].wins++;
-                        stats[team1].losses++;
-                    }
-                }
-            }
-            return stats;
-        }
-        main.getStats2015 = function() {
-            $http({
-                    method: 'GET',
-                    url: "https://raw.githubusercontent.com/openfootball/football.json/master/2015-16/en.1.json"
-                }).then(function(response) {
-                    main.results15 = getTeamStats(response);
-                },
-                function (error) {
-                    alert("An error has occured!\n Check the console.");
-                    console.log(error);
-                });
-        }();
-        main.getStats2016 = function() {
-            $http({
-                    method: 'GET',
-                    url: "https://raw.githubusercontent.com/openfootball/football.json/master/2016-17/en.1.json"
-                }).then(function(response) {
-                    main.results16 = getTeamStats(response);
-                },
-                function (error) {
-                    alert("An error has occured!\n Check the console.");
-                    console.log(error);
-                });
-        }();
-    }();
-}])
+     this.getStats = function() { debugger;
+         main.stats = {};
+         function getTeamStats(response, year) {
+             main.data = response.rounds;
+             var stats = main.stats;
+
+             for (var i in main.data) {
+                 for (var j in main.data[i].matches) {
+                     var match = main.data[i].matches[j];
+                     var team1 = match.team1.name;
+                     var team2 = match.team2.name;
+
+                     if (stats[team1] == null) {
+                         stats[team1] = {};
+                     }
+                     if (stats[team1][year] == null) {
+                         stats[team1][year] = {};
+                     }
+                     if (stats[team2] == null) {
+                         stats[team2] = {};
+                     }
+                     if (stats[team2][year] == null) {
+                         stats[team2][year] = {};
+                     }
+
+                     main.obj1 = stats[team1][year];
+                     if (main.obj1.matches) {
+                         main.obj1.matches++;
+                     } else {
+                         main.obj1.matches = 1;
+                     }
+
+                     if (main.obj1.goals) {
+                         main.obj1.goals += match.score1;
+                     } else {
+                         main.obj1.goals = match.score1;
+                     }
+
+                     main.obj2 = stats[team2][year];
+                     if (main.obj2.matches) {
+                         main.obj2.matches++;
+                     } else {
+                         main.obj2.matches = 1;
+                     }
+
+                     if (main.obj2.goals) {
+                         main.obj2.goals += match.score1;
+                     } else {
+                         main.obj2.goals = match.score2;
+                     }
+
+                     if (match.score1 > match.score2) {
+                         if (stats[team1][year].wins) {
+                             stats[team1][year].wins++;
+                             stats[team2][year].losses++;
+
+                         } else {
+                             stats[team1][year].wins = 1;
+                             stats[team2][year].losses = 1;
+                         }
+                     } else if (match.score2 > match.score1) {
+                         if (stats[team2][year].wins) {
+                             stats[team2][year].wins++;
+                             stats[team1][year].losses++;
+
+                         } else {
+                             stats[team2][year].wins = 1;
+                             stats[team1][year].losses = 1;
+                         }
+                     } else {
+                         if (stats[team1][year].ties) {
+                             stats[team1][year].ties++;
+                             stats[team2][year].ties++;
+                         } else {
+                             stats[team1][year].ties = 1;
+                             stats[team2][year].ties = 1;
+                         }
+                         main.obj1.winPercent = ((stats[team1][year].wins * 100) / (obj1.matches)).toFixed(2);
+                         main.obj2.lossPercent = ((stats[team2][year].losses * 100) / (obj2.matches)).toFixed(2);
+                         main.obj2.winPercent = ((stats[team2][year].wins * 100) / (obj2.matches)).toFixed(2);
+                         main.obj1.lossPercent = ((stats[team1][year].losses * 100) / (obj1.matches)).toFixed(2);
+
+                     }
+
+                 }
+             }
+
+         }
+         main.getStats2015 = function() {
+             $http({
+                 method: 'GET',
+                 url: "https://raw.githubusercontent.com/openfootball/football.json/master/2015-16/en.1.json"
+             }).then(function(response) {
+                     console.log(response);
+                     var year = '2015';
+                     getTeamStats(response, year);
+                 },
+                 function(error) {
+                     alert("An error has occured!\n Check the console.");
+                     console.log(error);
+                 });
+         }();
+
+         main.getStats2016 = function() {
+             $http({
+                 method: 'GET',
+                 url: "https://raw.githubusercontent.com/openfootball/football.json/master/2016-17/en.1.json"
+             }).then(function(response) {
+                     console.log(response);
+                     var year = '2016';
+                     getTeamStats(response, year);
+                 },
+                 function(error) {
+                     alert("An error has occured!\n Check the console.");
+                     console.log(error);
+                 });
+         }();
+     }
+
+ }]);
